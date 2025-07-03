f4c4e66e148c5ba9886a1889e74ef602c1b46a52
Pooja_Sirvi <sirvipooja178@gmail.com>
2025-07-03 19:31:39 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,16 +1,19 @@
 const screen = document.querySelector(".screen");
 let currentScreen = '';
 
+//get number and display on screen
 function getNumber(number) {
     currentScreen += number;
     screen.innerText = currentScreen;
 }
 
+//get operator and display on screen
 function getOperator(operator) {
     currentScreen += `${operator}`;
     screen.innerText = currentScreen;
 }
 
+//perform calculation
 function calculateResult() {
     try {
         currentScreen = eval(currentScreen);
@@ -20,11 +23,13 @@ function calculateResult() {
     }
 }
 
+//clear the screen
 function clearScreen() {
     currentScreen = '';
     screen.innerText = currentScreen;
 }
 
+//go back one step
 function backSpace() {
     currentScreen = currentScreen.slice(0, -1);
     screen.innerText = currentScreen;
